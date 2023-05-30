231b295ae148d867dd2683c9ab3e184f353e63ea
ian612 <34078802+ian612@users.noreply.github.com>
2023-05-30 17:28:15 -0400

    updated with first (probably) working gulpfile

---
+++ src/token-action-hud-sfrpg.js
@@ -0,0 +1,7 @@
+// System Module Imports
+import { ActionHandler } from './modules/action-handler.js'
+import { RollHandler as Core } from './modules/roll-handler.js'
+import { DEFAULTS } from './modules/defaults.js'
+import * as systemSettings from './modules/settings.js'
+
+export let SystemManager = null
